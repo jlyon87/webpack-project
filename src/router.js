7a84800f654aa47ddc444085abde93a39f91f609
jlyonas <jlyon@acumensolutions.com>
2017-12-07 09:28:01 -0500

    code splitting on react router

--- src/router.js
+++ src/router.js
@@ -3,20 +3,38 @@ import { Router, Route, IndexRoute, hashHistory } from 'react-router';
 
 import Home from './components/Home';
 import ArtistMain from './components/artists/ArtistMain';
-import ArtistDetail from './components/artists/ArtistDetail';
-import ArtistCreate from './components/artists/ArtistCreate';
-import ArtistEdit from './components/artists/ArtistEdit';
+
+const imports = {
+	"new": () => { return System.import("./components/artists/ArtistCreate"); },
+	details: () => { return System.import("./components/artists/ArtistDetail"); },
+	edit: () => { return System.import("./components/artists/ArtistEdit"); }
+}
+
+const getRouter = (importType, path) => {
+	return {
+		path,
+		getComponent(location, cb) {
+			imports[importType]()
+				.then(module => cb(null, module.default))
+				.catch(err => console.error);
+		}
+	}
+};
+
+const componentRoutes = {
+	component: Home,
+	path: '/',
+	indexRoute: { component: ArtistMain },
+	childRoutes: [
+		getRouter("new", "artists/new"),
+		getRouter("details", "artists/:id"),
+		getRouter("edit", "artists/:id/edit")
+	]
+};
 
 const Routes = () => {
   return (
-    <Router history={hashHistory}>
-      <Route path="/" component={Home}>
-        <IndexRoute component={ArtistMain} />
-        <Route path="artists/new" component={ArtistCreate} />
-        <Route path="artists/:id" component={ArtistDetail} />
-        <Route path="artists/:id/edit" component={ArtistEdit} />
-      </Route>
-    </Router>
+    <Router history={hashHistory} routes={componentRoutes} />
   );
 };
 
