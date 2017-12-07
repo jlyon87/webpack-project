8da1d32fc025cd198a20cd13d8e088b9a808dfe0
jlyonas <jlyon@acumensolutions.com>
2017-12-07 09:04:49 -0500

    init commit

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,51 @@
-var webpack = require('webpack');
-var path = require('path');
+const webpack = require('webpack');
+const path = require('path');
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+const VENDOR_LIBS = [
+	"faker",
+	"lodash",
+	"redux",
+	"react",
+	"react-redux",
+	"react-dom",
+	"react-input-range",
+	"redux-form",
+	"redux-thunk"
+];
 
 module.exports = {
-  entry: './src/index.js',
+  entry: {
+		bundle: "./src/index.js",
+		vendor: VENDOR_LIBS
+	},
   output: {
     path: path.join(__dirname, 'dist'),
-    filename: 'bundle.js'
-  }
+    filename: "[name].[chunkhash].js"
+  },
+
+	module: {
+		rules: [
+			{
+				use: "babel-loader",
+				test: /\.js$/,
+				exclude: /node_modules/
+			},
+			{
+				use: ["style-loader", "css-loader"],
+				test: /\.css$/
+			}
+		]
+	},
+
+	plugins: [
+		// remove VENDOR_LIBS duplicate dependencies from bundle.js
+		new webpack.optimize.CommonsChunkPlugin({
+			names: ["vendor", "manifest"]
+		}),
+		// Inject script dependencies to index.html for us
+		new HtmlWebpackPlugin({
+			template: "src/index.html"
+		})
+	]
 };
