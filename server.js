2740b146cdc3d2981a3fdb6043cf01c5c445672e
jlyonas <jlyon@acumensolutions.com>
2017-12-07 11:51:12 -0500

    change index.html path

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ if(process.env.NODE_ENV !== "production") {
 } else {
 	app.use(express.static("dist"));
 	app.get("*", (req, res) => {
-		res.sendFile(path.join( __dirname, "dist", "index.html"));
+		res.sendFile(path.join( __dirname, "dist/index.html"));
 	});
 }
 
